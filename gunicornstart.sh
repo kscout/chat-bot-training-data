184efce26e0698684b72fb1db240d40620d25793
pratikmahajan <pratik@mahajan.xyz>
2019-07-22 09:34:44 -0400

    added gunicorn startup script

---
+++ gunicornstart.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Prepare log files and start outputting logs to stdout
+mkdir -p logs
+touch logs/gunicorn.log
+# touch logs/gunicorn-access.log
+tail -n 0 -f logs/gunicorn*.log &
+
+
+exec gunicorn app:app \
+    --bind 0.0.0.0:8080 \
+    --workers 5 \
+    --log-level=info \
+    --log-file=logs/gunicorn.log \
+"$@"
